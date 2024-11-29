cf3c90731d80639cc335d806078ba74707f50db0
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2024-11-29 01:52:28 -0700

    pause menu now blurs background

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -9,9 +9,14 @@ func _input(event: InputEvent) -> void:
 	elif (event.is_action_pressed("Pause") && !get_tree().paused) :
 		pause()
 
+func _process(delta: float) -> void:
+	if (visible) :
+		$BlurEffect.material.set("shader_parameter/lod", lerp($BlurEffect.material.get("shader_parameter/lod"), 1.0, 3 * delta))
+
 func pause():
 	GameManager.PauseGame()
 	visible = true
+	$BlurEffect.material.set("shader_parameter/lod", 0.0)
 
 func resume():
 	GameManager.ResumeGame()
